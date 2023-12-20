37a1711b7a53f8becac9b8fcda4a40c1b441c656
arjun-b03 <abommade@gmail.com>
2023-12-20 02:46:41 +0100

    finish bxast

--- bxc/bxlib/bxast.py
+++ bxc/bxlib/bxast.py
@@ -1,26 +1,10 @@
 # --------------------------------------------------------------------
 import dataclasses as dc
 import enum
-
-from typing import Optional as Opt
+from typing import Optional as Opt, List, Tuple
 
 # ====================================================================
 # Parse tree / Abstract Syntax Tree
-
-# --------------------------------------------------------------------
-class Type(enum.Enum):
-    VOID = 0
-    BOOL = 1
-    INT  = 2
-
-    def __str__(self):
-        match self:
-            case self.VOID:
-                return 'void'
-            case self.INT:
-                return 'int'
-            case self.BOOL:
-                return 'bool'
 
 # --------------------------------------------------------------------
 @dc.dataclass
@@ -35,7 +19,7 @@ class Range:
 # --------------------------------------------------------------------
 @dc.dataclass
 class AST:
-    position: Opt[Range] = dc.field(kw_only = True, default = None)
+    position: Opt[Range] = dc.field(kw_only=True, default=None)
 
 # --------------------------------------------------------------------
 @dc.dataclass
@@ -43,9 +27,57 @@ class Name(AST):
     value: str
 
 # --------------------------------------------------------------------
+class Type():
+    pass
+
+class BasicBXType(Type, enum.Enum):
+    VOID = 0
+    BOOL = 1
+    INT = 2
+    NULL = 3  # Used when null pointer can't be typecast
+
+    def __str__(self):
+        match self:
+            case self.VOID:
+                return 'void'
+            case self.INT:
+                return 'int'
+            case self.BOOL:
+                return 'bool'
+            case self.NULL:
+                return 'null_ptr'
+
+@dc.dataclass
+class PointerType(Type):
+    target: Type
+    
+    def __str__(self):
+        return f"{self.target}*"
+
+@dc.dataclass
+class ArrayType(Type):
+    target: Type
+    size: int
+    
+    def __str__(self):
+        return f"{self.target}[{self.size}]"
+
+@dc.dataclass
+class StructType(Type):
+    attributes: list[Tuple[Name, Type]]
+    attr_lookup: Opt[dict[str, Tuple[int, Type]]] = dc.field(kw_only=True, default=None)
+
+    def __str__(self):
+        return f"struct"
+
+@dc.dataclass
+class StandinType(Type):
+    type_name: Name
+
+# --------------------------------------------------------------------
 @dc.dataclass
 class Expression(AST):
-    type_: Opt[Type] = dc.field(kw_only = True, default = None)
+    type_: Opt[Type] = dc.field(kw_only=True, default=None)
 
 # --------------------------------------------------------------------
 @dc.dataclass
@@ -64,9 +96,20 @@ class IntExpression(Expression):
 
 # --------------------------------------------------------------------
 @dc.dataclass
+class NullExpression(Expression):
+    pass
+
+# --------------------------------------------------------------------
+@dc.dataclass
 class OpAppExpression(Expression):
     operator: str
     arguments: list[Expression]
+
+# --------------------------------------------------------------------
+@dc.dataclass
+class AllocExpression(Expression):
+    alloctype: Type
+    size: Expression
 
 # --------------------------------------------------------------------
 @dc.dataclass
@@ -78,6 +121,43 @@ class CallExpression(Expression):
 @dc.dataclass
 class PrintExpression(Expression):
     argument: Expression
+
+# --------------------------------------------------------------------
+class Assignable(Expression):
+    pass
+
+# --------------------------------------------------------------------
+@dc.dataclass
+class VarAssignable(Assignable):
+    name: Name
+
+# --------------------------------------------------------------------
+@dc.dataclass
+class DerefAssignable(Assignable):
+    argument: Assignable
+
+# --------------------------------------------------------------------
+@dc.dataclass
+class ArrayAssignable(Assignable):
+    argument: Assignable
+    index: Expression
+
+# --------------------------------------------------------------------
+@dc.dataclass
+class AttributeAssignable(Assignable):
+    argument: Assignable
+    attribute: str
+
+# --------------------------------------------------------------------
+@dc.dataclass
+class AttrPointerAssignable(Assignable):
+    argument: Assignable
+    attribute: str
+
+# --------------------------------------------------------------------
+@dc.dataclass
+class RefExpression(Expression):
+    argument: Assignable
 
 # --------------------------------------------------------------------
 class Statement(AST):
@@ -93,7 +173,7 @@ class VarDeclStatement(Statement):
 # --------------------------------------------------------------------
 @dc.dataclass
 class AssignStatement(Statement):
-    lhs: Name
+    lhs: Assignable
     rhs: Expression
 
 # --------------------------------------------------------------------
@@ -150,14 +230,20 @@ class GlobVarDecl(TopDecl):
     init: Expression
     type_: Type
 
-#--------------------------------------------------------------------
+# --------------------------------------------------------------------
 @dc.dataclass
 class ProcDecl(TopDecl):
     name: Name
-    arguments: list[tuple[Name, Type]]
+    arguments: list[Tuple[Name, Type]]
     rettype: Opt[Type]
     body: Statement
 
 # --------------------------------------------------------------------
-Block   = list[Statement]
+@dc.dataclass
+class TypedefDecl(TopDecl):
+    alias: Name
+    original_type: Type
+
+# --------------------------------------------------------------------
+Block = list[Statement]
 Program = list[TopDecl]
